a91e1f0754d1906cb7c39a745667666f9c613cf9
Alex <72357559+AlexKiriak@users.noreply.github.com>
2020-12-21 23:14:05 +0200

    'update' and 'delete' added

--- ordr/urls.py
+++ ordr/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^order$', views.order_list)
+    url(r'^order$', views.order_list),
+    url(r'^order/(?P<order_id>[0-9]+)$', views.order_detail)
 ]
--- ordr/views.py
+++ ordr/views.py
@@ -11,7 +11,7 @@ from ordr.serializers import OrdrSerializer
 @api_view(['GET', 'POST'])
 def order_list(request):
     if request.method == 'GET':
-        orders = Ordr.objects.values("order_id")
+        orders = Ordr.objects.all()
 
         orders_serializer = OrdrSerializer(orders, many=True)
         return JsonResponse(orders_serializer.data, safe=False)
@@ -22,3 +22,25 @@ def order_list(request):
             order_serializer.save()
             return JsonResponse(order_serializer.data, status=status.HTTP_201_CREATED)
         return JsonResponse(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def order_detail(request, order_id):
+    try:
+        orders = Ordr.objects.get(order_id=order_id)
+    except Ordr.DoesNotExist:
+        return JsonResponse({'message:': 'Order does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        order_serializer = OrdrSerializer(orders)
+        return JsonResponse(order_serializer.data)
+    elif request.method == 'PUT':
+        order_data = JSONParser().parse(request)
+        order_serializer = OrdrSerializer(orders, data=order_data)
+        if order_serializer.is_valid():
+            order_serializer.save()
+            return JsonResponse(order_serializer.data)
+        return JsonResponse(order_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        orders.delete()
+        return JsonResponse({'message': 'Order was deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
